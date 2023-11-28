bade1d909217d48b913bd85c0cdfd4739cfb6709
Catoxi <89002045+CatoxiRetreater@users.noreply.github.com>
2023-11-29 02:32:26 +0530

    Add files via upload

---
+++ exp5_1.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<math.h>
+
+int armstrong (int num)
+{
+int originalNum, remainder, n = 0;
+   float result = 0.0;
+   originalNum = num;
+   for (originalNum = num; originalNum != 0; ++n) 
+   {
+    originalNum /= 10;
+   }
+   for (originalNum = num; originalNum != 0; originalNum /= 10) 
+   {
+    remainder = originalNum % 10;
+    result += pow(remainder, n);
+   }
+   if ((int)result == num)
+    printf("%d is an Armstrong number.\n", num);
+   else
+    printf("%d is not an Armstrong number.\n", num);
+   return num;
+}
+int coprime(int num)
+{
+    int remainder,n,reverse,gcd;
+    n=num;
+    reverse=0;
+    while (n != 0) {
+    remainder = n % 10;
+    reverse = reverse * 10 + remainder;
+    n /= 10;
+    }
+    for(int i=1; i <= num || i <= reverse; ++i)
+    {
+        if(num%i==0 && reverse%i==0)
+            gcd = i;
+    }
+    
+    if(gcd==1)
+    {
+        printf("The number is coprime\n");
+    }
+    else
+    {
+        printf("The number is not a coprime\n");
+    }
+}
+
+int factorial(int num)
+{
+    if (num == 0) 
+    {
+        return 0;
+    } 
+    else if(num==1)
+    {
+        return 1;
+    } 
+    else
+    {
+        return num * factorial(num - 1);
+    }
+}
+
+void main()
+{
+    int a, fact;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    armstrong(a);
+    coprime(a);
+    fact=factorial(a);
+    printf("Factorial: %d\n", fact);
+}
